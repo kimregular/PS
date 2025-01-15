5fab1897b1cec75f8a0214b63ba57ec963d6299c
regular_kim <regular.jk.kim@gmail.com>
2025-01-15 23:16:29 +0900

    BJ : https://www.acmicpc.net/problem/2739

---
+++ src/p2739/Main.java
@@ -0,0 +1,40 @@
+package src.p2739;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            Solution s = new Solution();
+            System.out.println(s.solution(readInput(br)));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int readInput(BufferedReader br) throws IOException {
+        return Integer.parseInt(br.readLine());
+    }
+}
+
+class Solution {
+
+    public String solution(int target) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 1; i <= 9; i++) {
+            answer.append(target).append(" * ").append(i)
+                  .append(" = ").append(target * i).append("\n");
+        }
+        return answer.toString();
+    }
+}
