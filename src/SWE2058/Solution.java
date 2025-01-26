c55b91a1eb22f3cd56bb560a5160a5304bbacdde
kimregular <regular.jk.kim@gmail.com>
2025-01-26 20:08:45 +0900

    SWE : 2058

---
+++ src/SWE2058/Solution.java
@@ -0,0 +1,34 @@
+package src.SWE2058;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution().run();
+    }
+
+    private void run() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            int result = 0;
+            int target = readInput(br);
+
+            while (target > 0) {
+                result += target % 10;
+                target /= 10;
+            }
+
+            System.out.println(result);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int readInput(BufferedReader br) throws IOException {
+        return Integer.parseInt(br.readLine());
+    }
+}
