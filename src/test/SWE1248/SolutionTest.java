98b738910f9d28f7ef949d07c69bc83ace316ca5
regular_kim <regular.jk.kim@gmail.com>
2025-02-06 16:35:16 +0900

    modified : comments

--- src/test/SWE1248/SolutionTest.java
+++ src/test/SWE1248/SolutionTest.java
@@ -12,39 +12,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SolutionTest {
 
 	private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+	// 출력 결과를 확인할 outputstream
 	private final PrintStream originalSystemOut = System.out;
+	// 현재 출력 나가는 stream
 
 	@BeforeEach
 	public void setUp() {
-		// Redirect System.out to capture the output
 		System.setOut(new PrintStream(outputStream));
+		// 현재 출력되는 stream을 바꾼다.
+		// 즉, 콘솔에 출력되는 결과를 outputStream으로 리디렉션 한다.
 	}
 
 	@Test
 	void testFromFile() throws IOException {
-		// Define the file paths for input and output
+
 		String inputFilePath = "src/test/SWE1248/input.txt";
 		String expectedOutputFilePath = "src/test/SWE1248/output.txt";
+		// 입력 파일과 예상 출력 파일을 읽어온다.
 
-		// Read input and expected output from files
+
 		String input = new String(Files.readAllBytes(Paths.get(inputFilePath)));
 		String expectedOutput = new String(Files.readAllBytes(Paths.get(expectedOutputFilePath)));
 
 		input = input.replaceAll("\r\n", "\n");
 		expectedOutput = expectedOutput.replaceAll("\r\n", "\n");
+		// 윈도우, 맥 줄바꿈 호환성 신경쓰기
 
-		// Run the test
-		// Simulate the input stream by writing the input to System.in
+
+
 		InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+		// 입력 파일 내용을 바이트 배열로 변환하여 InputStream으로 변경"
 		System.setIn(inputStream);
+		// 시스템에 인풋스트림 주입
 
-		// Run the solution
+
 		new src.main.SWE1248.Solution().run();
+		// 이 코드에서는 원래 콘솔에 출력되어야 하는 결과가
+		// outputStream으로 리디렉션되므로
+		// 콘솔에 출력되지 않고 outputStream에서 결과가 출력된다.
 
-		// Capture the output and compare it with the expected output
+
 		assertEquals(expectedOutput, outputStream.toString().trim());
+		// output.txt 와 코드 결과 비교
 
-		// Reset System.out
 		System.setOut(originalSystemOut);
+		// 출력 스트림을 원래의 시스템 출력으로 리셋
 	}
 }
