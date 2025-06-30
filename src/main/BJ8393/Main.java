7d7fe61cd42f374f604f7fdb581eeecf14f35245
kimregular <regular.jk.kim@gmail.com>
2025-06-30 19:54:41 +0900

    BJ : https://www.acmicpc.net/problem/8393

---
+++ src/main/BJ8393/Main.java
@@ -0,0 +1,37 @@
+package main.BJ8393;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        System.out.println(new Solution().solution(readInput()));
+    }
+
+    private int readInput() {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            return Integer.parseInt(br.readLine());
+
+        }catch(IOException e ) {
+            throw new RuntimeException();
+        }
+    }
+}
+
+class Solution {
+
+    public int solution(int target) {
+        int result = 0;
+
+        for(int i = 1; i <= target; i++) result+= i;
+
+        return result;
+    }
+}
