33bcc0b11a58da30c5eccf0cfdeeea46f967d1e2
kimregular <regular.jk.kim@gmail.com>
2025-09-07 15:05:27 +0900

    BJ : https://www.acmicpc.net/problem/9251

---
+++ src/main/BJ9251/Main.java
@@ -0,0 +1,41 @@
+package main.BJ9251;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static String str1;
+    static String str2;
+    static int[][] dp;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        str1 = br.readLine();
+        str2 = br.readLine();
+        dp = new int[str1.length() + 1][str2.length() + 1];
+
+        for(int[] d : dp) Arrays.fill(d, -1);
+
+        System.out.println(calc(str1.length(), str2.length()));
+
+        br.close();
+    }
+
+    static int calc(int i, int j) {
+        if (i == 0 || j == 0) {
+            return 0;
+        } else if (dp[i][j] != -1) {
+            return dp[i][j];
+        } else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+            dp[i][j] = calc(i - 1, j - 1) + 1;
+            return dp[i][j];
+        } else {
+            dp[i][j] = Math.max(calc(i - 1, j), calc(i, j - 1));
+            return dp[i][j];
+        }
+    }
+}
