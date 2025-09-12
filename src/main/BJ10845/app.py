6ef64786a04bba076de0e8d7ce6924a6e5966958
regular.jk.kim@gmail.com <regular.jk.kim@gmail.com>
2025-09-12 17:48:32 +0900

    BJ : https://www.acmicpc.net/problem/10845

---
+++ src/main/BJ10845/app.py
@@ -0,0 +1,22 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+q = deque()
+
+n = int(input())
+
+for _ in range(n):
+    command = input().split()
+    if command[0] == 'push':
+        q.append(int(command[1]))
+    elif command[0] == 'pop':
+        print(q.popleft() if q else -1)
+    elif command[0] == 'size':
+        print(len(q))
+    elif command[0] == 'empty':
+        print(0 if q else 1)
+    elif command[0] == 'front':
+        print(q[0] if q else -1)
+    elif command[0] == 'back':
+        print(q[-1] if q else -1)
